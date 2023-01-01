a79c2fc0a9a55b4ef312b0256c7188cad8fa5b42
Deepak R <111674608+Dipaque@users.noreply.github.com>
2023-01-01 22:28:44 +0530

    Create Diagnol difference.java
    Hacker Rank solution for diagnol differrence

---
+++ Diagnol difference.java
@@ -0,0 +1,25 @@
+import java.util*;
+public class Diagnol{
+public static void main(string[] args){
+int sumr=0;
+	int suml=0;
+int[][] a={{5,8},{3,4}};
+for(int i=0;i<a.length;i++) {
+	for(int j=i;j==i;j++) {
+		sumr=sumr+a[i][j];
+	}
+}
+for(int i=0;i<a.length;i++) {
+	for(int j=0;j<a.length;j++) {
+		if((i+j)== (a.length-1)) {
+		suml=suml+a[i][j];
+	}
+	}
+}
+int diff=Math.abs(sumr-suml);
+
+System.out.print("the ans is="+diff);
+
+
+	}
+ }
